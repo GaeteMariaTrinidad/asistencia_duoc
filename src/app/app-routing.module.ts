02d51718a719e2a8c7183e4389672f12d43a0237
Trinidad <mar.gaetem@duocuc.cl>
2023-09-03 20:42:16 -0300

    Primer Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'principal',
+    loadChildren: () => import('./pages/principal/principal.module').then( m => m.PrincipalPageModule)
+  },
+  {
+    path: 'restablecer',
+    loadChildren: () => import('./pages/restablecer/restablecer.module').then( m => m.RestablecerPageModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  mdl_usuario: string = '';
+  mdl_contrasena: string = '';
+
+  isAlertOpen = false;
+  alertButtons = ['OK'];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  navegar(){
+    if(this.mdl_usuario == 'ALBERTO' && this.mdl_contrasena == '12345'){
+
+      let parametros: NavigationExtras = {
+        state: {
+          user: this.mdl_usuario,
+          pass: this.mdl_contrasena
+        }
+      }
+
+      this.router.navigate(['principal'], parametros);
+    } else {
+      this.isAlertOpen = true;
+    }
+
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+
+}
